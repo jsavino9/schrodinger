94a083cdec1c9780705074cc9752d6a6808b19e4
Jsavino <jsavino@u.rochester.edu>
2018-12-19 13:26:21 -0500

    Added test functions and updated travis

--- schrodinger/__init__.py
+++ schrodinger/__init__.py
@@ -5,3 +5,5 @@
 __author__ = """James Savino"""
 __email__ = 'jsavino@u.rochester.edu'
 __version__ = '0.1.0'
+
+from schrodinger.schrodinger import *
--- tests/test_schrodinger.py
+++ tests/test_schrodinger.py
@@ -4,35 +4,53 @@
 """Tests for `schrodinger` package."""
 
 import pytest
-
-from click.testing import CliRunner
-
+import numpy as np
+import tensorflow as tf
+import math
 from schrodinger import schrodinger
 from schrodinger import cli
+tf.enable_eager_execution()
 
 
 @pytest.fixture
-def response():
-    """Sample pytest fixture.
 
-    See more at: http://doc.pytest.org/en/latest/fixture.html
-    """
-    # import requests
-    # return requests.get('https://github.com/audreyr/cookiecutter-pypackage')
+def test_start():
+	'''
+	empty space
+	'''
+def test_makearray():
+	x = [0,math.pi/2,math.pi]
+	b = 3
+	m = schrodinger.makearray(x,b)
+	assert int(m[0][0]) == int(math.cos(0))
+	assert int(m[1][0]) == int(math.sin(0))
+	assert int(m[1][1]) == int(math.sin(math.pi/2))
 
+def test_coeffs():
+	x = [0,math.pi/2,math.pi]
+	b = 3
+	v = [0,6,0]
+	coefs = schrodinger.v0hat(x,v,b)
+	assert int(coefs[0]) == int(0)
+	assert int(coefs[1]) == int(6)
+	assert int(coefs[2]) == int(0)
 
-def test_content(response):
-    """Sample pytest test function with the pytest fixture as an argument."""
-    # from bs4 import BeautifulSoup
-    # assert 'GitHub' in BeautifulSoup(response.content).title.string
+def test_h():
+	x = [0,math.pi/2,math.pi]
+	b = 3
+	v = [0,6,0]
+	c = 0
+	coefs = schrodinger.v0hat(x,v,b)
+	h = schrodinger.hamiltonian(x,c,coefs,b)
+	assert int(h[0][0]) == int(0)
+	assert int(h[1][0]) == int(6)
 
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'schrodinger.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
+def test_e():
+	x = [0,math.pi/2,math.pi]
+	b = 3
+	v = [0,6,0]
+	c = 0
+	coefs = schrodinger.v0hat(x,v,b)
+	h = schrodinger.hamiltonian(x,c,coefs,b)
+	emin,cfmin = schrodinger.energies(h)
+	assert(float(emin)) == float(-3.708204746246338)
